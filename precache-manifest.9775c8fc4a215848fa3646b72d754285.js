73d936a6f6c3263d20491d8645fad1357d1e6961
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2021-04-18 08:48:51 +0300

    Add files via upload

--- precache-manifest.9775c8fc4a215848fa3646b72d754285.js
+++ precache-manifest.9775c8fc4a215848fa3646b72d754285.js
@@ -1,126 +1,126 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "2d0d4473d4733914731d",
-    "url": "/css/app.68a17d98.css"
+    "url": "colors/css/app.68a17d98.css"
   },
   {
     "revision": "6ffbefc66468b90d7af1cbe1e9f13430",
-    "url": "/fonts/Inter-Light.6ffbefc6.ttf"
+    "url": "colors/fonts/Inter-Light.6ffbefc6.ttf"
   },
   {
     "revision": "5ff1f2a9a78730d7d0c309320ff3c9c7",
-    "url": "/fonts/Inter-Medium.5ff1f2a9.ttf"
+    "url": "colors/fonts/Inter-Medium.5ff1f2a9.ttf"
   },
   {
     "revision": "515cae74eee4925d56e6ac70c25fc0f6",
-    "url": "/fonts/Inter-Regular.515cae74.ttf"
+    "url": "colors/fonts/Inter-Regular.515cae74.ttf"
   },
   {
     "revision": "ec60b23f3405050f546f4765a9e90fec",
-    "url": "/fonts/Inter-SemiBold.ec60b23f.ttf"
+    "url": "colors/fonts/Inter-SemiBold.ec60b23f.ttf"
   },
   {
     "revision": "e7876d7c1d4ae3086d4f9aa3aab28412",
-    "url": "/img/logo.svg"
+    "url": "colors/img/logo.svg"
   },
   {
     "revision": "89b2f15aafb0587dc3e08868ace78161",
-    "url": "/img/products/1.jpg"
+    "url": "colors/img/products/1.jpg"
   },
   {
     "revision": "381018b42f1929cd196fb5db061adff4",
-    "url": "/img/products/10.jpg"
+    "url": "colors/img/products/10.jpg"
   },
   {
     "revision": "2cc5e9324d31404a172c63c5372d18cf",
-    "url": "/img/products/11.jpg"
+    "url": "colors/img/products/11.jpg"
   },
   {
     "revision": "155a48681a6a57be896026609e8f815a",
-    "url": "/img/products/12.jpg"
+    "url": "colors/img/products/12.jpg"
   },
   {
     "revision": "ac1a1c1a1b90c144c09fff561071bab8",
-    "url": "/img/products/13.jpg"
+    "url": "colors/img/products/13.jpg"
   },
   {
     "revision": "dabc3a3684914cf6f193c9092aa94c73",
-    "url": "/img/products/14.jpg"
+    "url": "colors/img/products/14.jpg"
   },
   {
     "revision": "381018b42f1929cd196fb5db061adff4",
-    "url": "/img/products/15.jpg"
+    "url": "colors/img/products/15.jpg"
   },
   {
     "revision": "eff7e742da9ef47059512ccfefbb7d91",
-    "url": "/img/products/2.jpg"
+    "url": "colors/img/products/2.jpg"
   },
   {
     "revision": "c2675605eff3acb82363fbdebe5df366",
-    "url": "/img/products/3.jpg"
+    "url": "colors/img/products/3.jpg"
   },
   {
     "revision": "0a90a74b6513ac4ef9da645a61bd02cd",
-    "url": "/img/products/4.jpg"
+    "url": "colors/img/products/4.jpg"
   },
   {
     "revision": "db1f87b7ff2b179c17c1323af3da19a0",
-    "url": "/img/products/5.jpg"
+    "url": "colors/img/products/5.jpg"
   },
   {
     "revision": "2cc5e9324d31404a172c63c5372d18cf",
-    "url": "/img/products/6.jpg"
+    "url": "colors/img/products/6.jpg"
   },
   {
     "revision": "155a48681a6a57be896026609e8f815a",
-    "url": "/img/products/7.jpg"
+    "url": "colors/img/products/7.jpg"
   },
   {
     "revision": "ac1a1c1a1b90c144c09fff561071bab8",
-    "url": "/img/products/8.jpg"
+    "url": "colors/img/products/8.jpg"
   },
   {
     "revision": "dabc3a3684914cf6f193c9092aa94c73",
-    "url": "/img/products/9.jpg"
+    "url": "colors/img/products/9.jpg"
   },
   {
     "revision": "20ba2790f06bdda71511d4783dd406a0",
-    "url": "/img/slider_img1.jpg"
+    "url": "colors/img/slider_img1.jpg"
   },
   {
     "revision": "4f16a6e20ea5b089e5866514a12e72c5",
-    "url": "/index.html"
+    "url": "colors/index.html"
   },
   {
     "revision": "a3fca2b18cc7f46aa33d",
-    "url": "/js/about.12751540.js"
+    "url": "colors/js/about.12751540.js"
   },
   {
     "revision": "2d0d4473d4733914731d",
-    "url": "/js/app.7dcb60f1.js"
+    "url": "colors/js/app.7dcb60f1.js"
   },
   {
     "revision": "e9889eade8a664c44066",
-    "url": "/js/chunk-vendors.3e95c8c7.js"
+    "url": "colors/js/chunk-vendors.3e95c8c7.js"
   },
   {
     "revision": "8a8bd9d7b5bd43704442e69758170aa3",
-    "url": "/manifest.json"
+    "url": "colors/manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "colors/robots.txt"
   },
   {
     "revision": "64791b9877715df6fc1998873e95c0b6",
-    "url": "/server/cart.json"
+    "url": "colors/server/cart.json"
   },
   {
     "revision": "a47b280501deafa537f759cb23c78c65",
-    "url": "/server/products.json"
+    "url": "colors/server/products.json"
   },
   {
     "revision": "bbd3acb1e1e75cac5ed5e59a5f8c1a44",
-    "url": "/server/slides.json"
+    "url": "colors/server/slides.json"
   }
 ]);
